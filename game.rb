7e66e7c90f5cd969553a2df350a1c4e7a97be7ec
devlin-person <devylin.91@gmail.com>
2017-12-05 11:22:39 -0500

    Game can be played multiple times in a row

--- game.rb
+++ game.rb
@@ -1,37 +1,60 @@
-puts "Let's play Rock, Paper, Scissors! \nReady? \nPick 'r' for rock, 'p' for paper, or 's' for scissors. \nRock, paper, scissors, shoot!"
+puts "Would you like to play Rock, Paper, Scissors? (y/n)"
 
-player = gets.chomp.downcase
+answer = gets.chomp.downcase
 
-
-while player != "r" && player != "p" && player != "r"
-  puts "Please select rock (r), paper (p), or scissors (s)."
-  player = gets.chomp.downcase
+while answer != "y" && answer != "n"
+  puts "Please select yes (y) or no (n)."
+  answer = gets.chomp.downcase
 end
 
-if player == "r"
-  player_hand = "rock"
-elsif player == "p"
-  player_hand = " paper"
-elsif player == "s"
-  player_hand = "scissors"
-end
-
-computer = rand(3)
-
-if computer == 0
-  computer_hand = "rock"
-elsif computer == 1
-  computer_hand = "paper"
-elsif computer == 2
-  computer_hand = "scissors"
-end
-
-if player == "r" && computer == 1 || player == "s" && computer == 0 || player == "p" && computer == 2
-  winner = "Computer wins!"
-elsif player == "r" && computer == 2 || player == "p" && computer == 0 || player == "s" && computer == 1
-  winner = "Player wins!"
+if answer == "n"
+  puts "That's too bad."
 else
-  winner = "Tie!"
-end
+  while answer != "n"
+    puts "Let's play Rock, Paper, Scissors! \nReady? \nPick 'r' for rock, 'p' for paper, or 's' for scissors. \nRock, paper, scissors, shoot!"
 
-puts "You picked #{player_hand}. \nComputer picked #{computer_hand}. \n#{winner}"
+    player = gets.chomp.downcase
+
+    while player != "r" && player != "p" && player != "r"
+      puts "Please select rock (r), paper (p), or scissors (s)."
+      player = gets.chomp.downcase
+    end
+
+    if player == "r"
+      player_hand = "rock"
+    elsif player == "p"
+      player_hand = " paper"
+    elsif player == "s"
+      player_hand = "scissors"
+    end
+
+    computer = rand(3)
+
+    if computer == 0
+      computer_hand = "rock"
+    elsif computer == 1
+      computer_hand = "paper"
+    elsif computer == 2
+      computer_hand = "scissors"
+    end
+
+    if player == "r" && computer == 1 || player == "s" && computer == 0 || player == "p" && computer == 2
+      winner = "Computer wins!"
+    elsif player == "r" && computer == 2 || player == "p" && computer == 0 || player == "s" && computer == 1
+      winner = "Player wins!"
+    else
+      winner = "Tie!"
+    end
+
+    puts "Play again? (y/n)"
+
+    answer = gets.chomp.downcase
+
+    while answer != "y" && answer != "n"
+      puts "Please select yes (y) or no (n)."
+      answer = gets.chomp.downcase
+    end
+  end
+
+  puts "That's for playing!"
+end
